676e9caeb36873c02c68cf98686c0788031ab304
Seth McLean <seth-mclean@idexx.com>
2024-02-04 04:52:00 +0000

    added lsp support and trouble.nvim

--- nvim/.config/nvim/lua/seth/init.lua
+++ nvim/.config/nvim/lua/seth/init.lua
@@ -1,3 +1,23 @@
 require("seth.remap")
 require("seth.packages")
 print("Running seth/init.lua")
+
+-- Tab settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = -1
+
+-- Show line numbers
+vim.opt.number = true
+
+-- Draw whitespace and page width
+vim.opt.listchars = {
+  eol = 'ꜜ',
+  space = '·',
+  trail = '✚',
+  extends = '▶',
+  precedes = '◀',
+}
+vim.opt.list = true
+vim.opt.colorcolumn = "80"
--- nvim/.config/nvim/lua/seth/packages.lua
+++ nvim/.config/nvim/lua/seth/packages.lua
@@ -24,12 +24,49 @@ plugins = {
       end)
     end,
   },
+  {
+    "folke/trouble.nvim",
+     dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
   { 
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-	vim.cmd.colorscheme("gruvbox")
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    lazy = false,
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup{
+        ensure_installed = {
+          -- "clangd",
+          -- "csharp_ls",
+          -- "java_language_server",
+          "lua_ls",
+          "rust_analyzer",
+          "pylsp",
+          "gopls",
+          "dockerls",
+          "helm_ls",
+          "html",
+          "kotlin_language_server",
+          "jsonls",
+          "tsserver",
+          "marksman",
+          "taplo",
+          "terraformls",
+          "lemminx",
+          "yamlls",
+        }
+      }
     end,
   },
   {
@@ -37,40 +74,40 @@ plugins = {
     build = ":TSUpdate",
     config = function()
       require'nvim-treesitter.configs'.setup {
-  	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-  	ensure_installed = { 
-		"c",
-		"rust",
-		"go",
-		"python",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"yaml",
-		"terraform",
-		"json",
-		"javascript",
-		"typescript",
-		"css",
-		"toml",
-		"xml",
-		"html",
-	},
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { 
+      "c",
+      "rust",
+      "go",
+      "python",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "yaml",
+      "terraform",
+      "json",
+      "javascript",
+      "typescript",
+      "css",
+      "toml",
+      "xml",
+      "html",
+    },
 
-  	-- Install parsers synchronously (only applied to `ensure_installed`)
-  	sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  	-- Automatically install missing parsers when entering buffer
-  	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  	auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  	-- List of parsers to ignore installing (or "all")
-  	ignore_install = {},
-  	highlight = {
-    	  enable = true,
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = {},
+    highlight = {
+        enable = true,
           disable = {},
-	  additional_vim_regex_highlighting = false,
-  	},
+    additional_vim_regex_highlighting = false,
+    },
       }
     end
   },
