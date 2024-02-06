5aa321c37d1edf6f8e6c416e95c3ea9110547fc9
Seth McLean <seth-mclean@idexx.com>
2024-02-06 18:19:44 +0000

    fixed type issue in packages.lua, fixed tmux config, added bootstrapping of package manager to tmux config

--- nvim/.config/nvim/lua/seth/packages.lua
+++ nvim/.config/nvim/lua/seth/packages.lua
@@ -47,7 +47,7 @@ local plugins = {
     config = function()
       require("neodev").setup({
         library = { plugins = { "nvim-dap-ui" }, types = true }})
-      require("nvim-dap-virtual-text").setup()
+      require("nvim-dap-virtual-text").setup({})
       require("dapui").setup()
     end,
   },
