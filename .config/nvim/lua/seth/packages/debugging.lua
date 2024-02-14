f94d11e527cddd9a0313bbd376d53333af4ce8c1
Seth McLean <seth-mclean@idexx.com>
2024-02-14 03:40:29 +0000

    adding neotest, indent lines, and more telescope bindings

--- .config/nvim/lua/seth/packages/debugging.lua
+++ .config/nvim/lua/seth/packages/debugging.lua
@@ -1,125 +1,126 @@
-  -- Debugger setup
+-- Debugger setup
 return {
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      dap.adapters.go = {
-        type = "server",
-        port = "${port}",
-        executable = {
-          command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
-          args = { "dap", "-l", "127.0.0.1:${port}" },
-        },
-      }
-      dap.configurations.rust = {
-         {
-          name = "LLDB: Launch",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            local output = vim.fn.systemlist("cargo build -q --message-format=json 2>1")
-            for _, l in ipairs(output) do
-              local json = vim.json.decode(l)
-              if json == nil then
-                error("error parsing json")
-              end
-              if json.success == false then
-                return error("error building package")
-              end
-              if json.executable ~= nil then
-                return json.executable
-              end
-            end
-          end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-          args = {},
-        },
-      }
-    end
-  },
-  {
-    "folke/neodev.nvim",
-    config = function()
-      require("neodev").setup({
-      library = { plugins = { "nvim-dap-ui" }, types = true }})
-    end
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      require("nvim-dap-virtual-text").setup({})
-    end
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "theHamsta/nvim-dap-virtual-text",
-      "folke/neodev.nvim",
-    },
-    config = function()
-      require("dapui").setup()
-      local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "williamboman/mason.nvim",
-    },
-    config = function()
-      require("mason-nvim-dap").setup({
-      ensure_installed = {'delve', 'python', 'codelldb'},
-      automatic_installation = true,
-      automatic_setup = true,
-      handlers = {
-          function(config)
-            -- all sources with no handler get passed here
-            -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
-          end,
-          python = function(config)
-              config.adapters = {
-                type = "executable",
-                command = "python",
-                args = {
-                  "-m",
-                  "debugpy.adapter",
-                },
-              }
-              require('mason-nvim-dap').default_setup(config) -- don't forget this!
-          end,
-          delve = function(config)
-            config.adapters = {
-              type = "server",
-              port = "${port}",
-              executable = {
-                command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
-                args = { "dap", "-l", "127.0.0.1:${port}" },
-              },
-            }
-              require('mason-nvim-dap').default_setup(config) -- don't forget this!
-          end,
-        },
-      })
-    end,
-  },
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			dap.adapters.go = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+					args = { "dap", "-l", "127.0.0.1:${port}" },
+				},
+			}
+			dap.configurations.rust = {
+				{
+					name = "LLDB: Launch",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						local output = vim.fn.systemlist("cargo build -q --message-format=json 2>1")
+						for _, l in ipairs(output) do
+							local json = vim.json.decode(l)
+							if json == nil then
+								error("error parsing json")
+							end
+							if json.success == false then
+								return error("error building package")
+							end
+							if json.executable ~= nil then
+								return json.executable
+							end
+						end
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+				},
+			}
+		end,
+	},
+	{
+		"folke/neodev.nvim",
+		config = function()
+			require("neodev").setup({
+				library = { plugins = { "nvim-dap-ui", "neotest" }, types = true },
+			})
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("nvim-dap-virtual-text").setup({})
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"theHamsta/nvim-dap-virtual-text",
+			"folke/neodev.nvim",
+		},
+		config = function()
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "delve", "python", "codelldb" },
+				automatic_installation = true,
+				automatic_setup = true,
+				handlers = {
+					function(config)
+						-- all sources with no handler get passed here
+						-- Keep original functionality
+						require("mason-nvim-dap").default_setup(config)
+					end,
+					python = function(config)
+						config.adapters = {
+							type = "executable",
+							command = "python",
+							args = {
+								"-m",
+								"debugpy.adapter",
+							},
+						}
+						require("mason-nvim-dap").default_setup(config) -- don't forget this!
+					end,
+					delve = function(config)
+						config.adapters = {
+							type = "server",
+							port = "${port}",
+							executable = {
+								command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+								args = { "dap", "-l", "127.0.0.1:${port}" },
+							},
+						}
+						require("mason-nvim-dap").default_setup(config) -- don't forget this!
+					end,
+				},
+			})
+		end,
+	},
 }
--- .config/nvim/lua/seth/packages/init.lua
+++ .config/nvim/lua/seth/packages/init.lua
@@ -1,68 +1,65 @@
-return  {
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {}
-    end,
-  },
-  -- Copilot setup
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function()
-        require("copilot").setup({})
-      end,
-    },
-    config = function ()
-      require("copilot_cmp").setup()
-    end
-  },
-  -- Undotree for local revisioning
-  {
-    'mbbill/undotree',
-    config = function()
-      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle,
-                      { desc='undotree toggle.'})
-    end,
-  },
-  -- Add telescope fuzzy finder
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files,
-                      { desc='telescope find files.'})
-      vim.keymap.set('n', '<leader>fg', builtin.git_files,
-                      { desc='telescope find git files.'})
-      vim.keymap.set('n', '<leader>fs', builtin.live_grep,
-                      { desc='telescope grep.'})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers,
-                      { desc='telescope find in buffers.'})
-      vim.keymap.set('n', '<leader>fc', builtin.commands,
-                      { desc='telescope find in commands.'})
-    end,
-  },
-  -- Add trouble to add lsp violatoin to gutter and quickfix
-  {
-    "folke/trouble.nvim",
-     dependencies = { "nvim-tree/nvim-web-devicons" },
-     opts = {}
-  },
-  -- Git integration
-  {
-    'lewis6991/gitsigns.nvim',
-    dependencies = { 'tpope/vim-fugitive' },
-    config = function()
-      require('gitsigns').setup()
-    end,
-  },
+return {
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	-- Copilot setup
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = {
+			"zbirenbaum/copilot.lua",
+			cmd = "Copilot",
+			event = "InsertEnter",
+			config = function()
+				require("copilot").setup({})
+			end,
+		},
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+	-- Undotree for local revisioning
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "undotree toggle." })
+		end,
+	},
+	-- Add telescope fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "telescope find files." })
+			vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "telescope find git files." })
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "telescope grep." })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "telescope find in buffers." })
+			vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "telescope find in commands." })
+			vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "telescope find in treesitter." })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "telescope find in keymaps." })
+		end,
+	},
+	-- Add trouble to add lsp violatoin to gutter and quickfix
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	-- Git integration
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "tpope/vim-fugitive" },
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 }
