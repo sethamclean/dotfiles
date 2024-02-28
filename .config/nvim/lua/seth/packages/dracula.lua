ffa170961ba7d5ae5b03f2ac17b20cd3058764c2
Seth McLean <seth-mclean@idexx.com>
2024-02-28 19:18:17 +0000

    changing background to black in dracula

--- .config/nvim/lua/seth/packages/dracula.lua
+++ .config/nvim/lua/seth/packages/dracula.lua
@@ -4,5 +4,7 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("dracula")
+		vim.api.nvim_set_hl(0, "Normal", { fg = "#ffffff", bg = "#101010" })
+		vim.api.nvim_set_hl(0, "SignColumn", { fg = "#ffffff", bg = "#101010" })
 	end,
 }
